0e2365f63066350229a8d201fe1b8c2b9fa26811
rrwiyatn <rey.reza@hotmail.com>
2016-02-07 01:04:30 -0500

    latest update for esp.js and index.html

--- Wireless-LED/NodeJS/esp.js
+++ Wireless-LED/NodeJS/esp.js
@@ -1,22 +1,18 @@
-
 var fs = require('fs');
 var app = require("express")();
-//var io = require('socket.io').listen(server);
 
-// Create a server
+// Create server
 var server = require('http').createServer(app);
+
+//listen to port 8081
 server.listen(8081, function(){
   console.log('listening on *:8081');
 });
 
+//open index.html
 app.get('/',function (request, response)
 {  
    response.sendFile(__dirname + '/index.html');
-})
-
-app.post('/',function (request, response)
-{  
-   console.log('RECEIVE POST');
 })
 
 console.log('Server running at http://127.0.0.1:8081/');
