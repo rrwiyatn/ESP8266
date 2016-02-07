b879a09cfff4b618b766c225a061eabb35d45e4d
rrwiyatn <rey.reza@hotmail.com>
2016-02-07 00:42:02 -0500

    latest update for init.lua and main.lua

--- Wireless-LED/NodeJS/init.lua
+++ Wireless-LED/NodeJS/init.lua
@@ -1,24 +1,20 @@
+-- Function declaration
 function wait_wifi()
-  --wifi.setmode(wifi.STATION)
-  wifi.sta.config("HP-Spectre360","17332E6D") 
-  print("Checking wifi... \n")
-  wifi.sta.connect()
-  wifi_ip = wifi.sta.getip()
-  if wifi_ip ~= "192.168.3.101" then
-    tmr.alarm(0, 2000,1,wait_wifi)
-    --print("Waiting WiFi... \n")
+  wifi.sta.connect() -- connect to AP
+  wifi_ip = wifi.sta.getip() -- get your ESP's IP address
+  if wifi_ip ~= "192.168.3.101" then --enter your ESP's IP address
+    tmr.alarm(0, 2000,1,wait_wifi) -- will recall wait_wifi function every 2 sec (2000 ms)
   else
-    wifi_connected = true
     print("Got IP "..wifi_ip.."\n")
     print("CONNECTED \n")
-    tmr.stop(0)
-    dofile("main.lua")
+    tmr.stop(0) -- stop the timer
+    dofile("main.lua") -- call our main.lua file
   end
 end
 
-print("Starting init.lua... \n")
-wifi.sta.disconnect()
-wifi.setmode(wifi.STATION)
-wifi.sta.config("HP-Spectre360","17332E6D") 
-wait_wifi()
+-- program starts here
+print("Starting init.lua... \n") -- print to your serial terminal (for debugging)
+wifi.setmode(wifi.STATION) -- set the module in STATION mode
+wifi.sta.config("HP-Spectre360","17332E6D") -- setup WiFi SSID and Password, wifi.sta.config("SSID","PASSWORD")
+wait_wifi() -- call wait_wifi function
 
--- Wireless-LED/NodeJS/main.lua
+++ Wireless-LED/NodeJS/main.lua
@@ -2,16 +2,17 @@ print("Starting main.lua... \n")
 
 --setup GPIO
 led = 3 --GPIO 0
-gpio.mode(led, gpio.OUTPUT)
+gpio.mode(led, gpio.OUTPUT) -- set GPIO 0 as an output
 
 --create ESP8266 server
-srv=net.createServer(net.TCP)
+srv=net.createServer(net.TCP) -- create TCP server
 print("Server created... \n")
 
---listening...
+--listening on port 80...
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
-        local buf = "";
+        -- declare variables
+        local pinState = "";
         local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
         if(method == nil)then
             _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
@@ -20,49 +21,37 @@ srv:listen(80,function(conn)
         if (vars ~= nil)then
             for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
                 _GET[k] = v
-            end
+            end 
         end
-        local _on,_off = "",""
-        if(_GET.pin == "ON")then --ledOn
+
+        --if receive get request "ON", turn LED on
+        if(_GET.pin == "ON")then
               gpio.write(led,gpio.HIGH)
-              buf=buf.."1"
+              pinState=pinState.."1"
               print("LED ON")
-              client:send(buf);
+              client:send(pinState);
               client:close();
               collectgarbage();
+              
+        --if receive get request "OFF", turn LED off     
         elseif(_GET.pin == "OFF")then --ledOff
               gpio.write(led,gpio.LOW)
-              buf=buf.."0"
+              pinState=pinState.."0"
               print("LED OFF")
-              client:send(buf);
+              client:send(pinState);
               client:close();
               collectgarbage();
-        elseif(_GET.pin == "STATS")then --send stats
-              --sendData()
-              print("Sending data...")
-              buf=buf.."<h1> HELLO </h1>"
-              --client=net.createConnection(net.TCP, 0);
-              --client:connect(8081,"http://127.0.0.1");
-              client:send(buf);
-              
+
+        --if receive get request "STATS", return pinState to Node.js server
+        elseif(_GET.pin == "STATS")then
+            --currently in progress
         end
-        --client:send(buf);
-        --client:close();
-        --collectgarbage();
     end)
+
+    -- will trigger when something is sent
     conn:on("sent",function(client)
           print("DATA SENT")
           client:close();
           collectgarbage();
     end)
-    --conn:on("disconnection",function(client)
-    --     print("DISCONNECTED")
-          --[[
-          client:close();
-          srv:close()
-          collectgarbage();
-          dofile("init.lua")
-           ]]--
-    --end)
-   
 end)
